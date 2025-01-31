09d6731b5aad294b30325773abaa722eeb2c7d0f
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2025-01-31 14:42:24 +0400

    SpawnContentRefactoring

--- Assets/Scripts/SpawnerContent/Spawner.cs
+++ Assets/Scripts/SpawnerContent/Spawner.cs
@@ -1,41 +1,34 @@
 using UnityEngine;
 
-public class Spawner : MonoBehaviour
+namespace SpawnerContent
 {
-    [SerializeField] private ItemPickUp[] _items; 
-    
-    public Enemy monsterPrefab;
-    public int numberOfMonsters = 3; 
-    public float spawnRadius = 5f; 
-    public Vector2 spawnAreaMin = new Vector2(-5, -5); 
-    public Vector2 spawnAreaMax = new Vector2(5, 5);
-
-    private Enemy _enemy;
-    
-    private void Start()
+    public class Spawner : MonoBehaviour
     {
-        SpawnMonsters();
-    }
+        [SerializeField] private ItemPickUp[] _items;
+        [SerializeField] private Enemy _monsterPrefab;
+        [SerializeField] private int _numberOfMonsters = 3;
+        [SerializeField] private float _spawnRadius = 5f;
+        [SerializeField] private Vector2 _spawnAreaMin;
+        [SerializeField] private Vector2 _spawnAreaMax;
 
-    private void SpawnMonsters()
-    {
-        for (int i = 0; i < numberOfMonsters; i++)
+        private Enemy _enemy;
+
+        private void Start()
         {
-            Vector2 randomPosition = new Vector2(
-                Random.Range(spawnAreaMin.x, spawnAreaMax.x),
-                Random.Range(spawnAreaMin.y, spawnAreaMax.y)
-            );
-            _enemy =  Instantiate(monsterPrefab, randomPosition, Quaternion.identity);
-            
-            
-            _enemy.SetItem(_items[Random.Range(0, _items.Length)]);
+            SpawnMonsters();
         }
-    }
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireCube(new Vector3((spawnAreaMin.x + spawnAreaMax.x) / 2, 0, (spawnAreaMin.y + spawnAreaMax.y) / 2),
-            new Vector3(spawnAreaMax.x - spawnAreaMin.x, 0, spawnAreaMax.y - spawnAreaMin.y));
+        private void SpawnMonsters()
+        {
+            for (int i = 0; i < _numberOfMonsters; i++)
+            {
+                Vector2 randomPosition = new Vector2(
+                    Random.Range(_spawnAreaMin.x, _spawnAreaMax.x),
+                    Random.Range(_spawnAreaMin.y, _spawnAreaMax.y));
+
+                _enemy = Instantiate(_monsterPrefab, randomPosition, Quaternion.identity);
+                _enemy.SetItem(_items[Random.Range(0, _items.Length)]);
+            }
+        }
     }
 }
